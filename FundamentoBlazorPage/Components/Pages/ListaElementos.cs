1215d8ab2b82fc29a926a450f91eec531289f3a4
Gabriel Montero <github.tic@sisalril.gob.do>
2025-02-10 14:55:14 -0400

    curso

---
+++ FundamentoBlazorPage/Components/Pages/ListaElementos.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace FundamentoBlazorPage.Components.Pages
+{
+    public partial class ListaElementos : ComponentBase
+    {
+        private List<string> elementos = new();
+        private string nuevoElemento; 
+
+        private void AgregarElementos()
+        {
+            if(!string.IsNullOrWhiteSpace(nuevoElemento))
+            {
+                elemento.Add(nuevoElemento);    
+                nuevoElemento = string.Empty;   
+            }
+        }
+    }
+}
